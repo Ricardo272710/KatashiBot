1200b706b906c920797f0fbc97b14206e89918ea
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-06-08 22:48:58 -0500

    Update 16vs16.js

--- plugins/16vs16.js
+++ plugins/16vs16.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, command, usedPrefix }) => {
 let ff = `https://telegra.ph/file/0ea56fa1fba7bdbb619ae.jpg`
 let vs16txt = `
 ╭──────⚔──────╮
-ㅤㅤ12 𝐕𝐄𝐑𝐒𝐔𝐒 12
+ㅤㅤ16 𝐕𝐄𝐑𝐒𝐔𝐒 16
 ╰──────⚔──────╯
 ╭──────────────╮
 │ㅤ⏱ 𝐇𝐎𝐑𝐀𝐑𝐈𝐎 
@@ -32,10 +32,18 @@ let vs16txt = `
 │⚜️ ➤ 
 │⚜️ ➤ 
 │
+│    𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 4
+│
+│👑 ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│
 │ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄:
 │⚜️ ➤ 
 │⚜️ ➤ 
 ╰─────────────╯
+               
 ` //Aquí arriba cambiar el texto sin borrar las comillas
 await conn.sendFile(m.chat, 'https://telegra.ph/file/0ea56fa1fba7bdbb619ae.jpg', 'fantasy.jpg', vs16txt.trim(),
 //En esta imagen vas a poner el url de la imagen grande
