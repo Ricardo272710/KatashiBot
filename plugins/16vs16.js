90f58147e0cc76743ccd06cd66dc5404a16d8de8
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-06-08 22:48:13 -0500

    Update 16vs16.js

--- plugins/16vs16.js
+++ plugins/16vs16.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, command, usedPrefix }) => {
 let ff = `https://telegra.ph/file/0ea56fa1fba7bdbb619ae.jpg`
 let vs16txt = `
 ╭──────⚔──────╮
-ㅤㅤ16 𝐕𝐄𝐑𝐒𝐔𝐒 16
+ㅤㅤ12 𝐕𝐄𝐑𝐒𝐔𝐒 12
 ╰──────⚔──────╯
 ╭──────────────╮
 │ㅤ⏱ 𝐇𝐎𝐑𝐀𝐑𝐈𝐎 
@@ -32,18 +32,10 @@ let vs16txt = `
 │⚜️ ➤ 
 │⚜️ ➤ 
 │
-│    𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 4
-│
-│👑 ➤ 
-│⚜️ ➤ 
-│⚜️ ➤ 
-│⚜️ ➤ 
-│
 │ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄:
 │⚜️ ➤ 
 │⚜️ ➤ 
 ╰─────────────╯
-               
 ` //Aquí arriba cambiar el texto sin borrar las comillas
 await conn.sendFile(m.chat, 'https://telegra.ph/file/0ea56fa1fba7bdbb619ae.jpg', 'fantasy.jpg', vs16txt.trim(),
 //En esta imagen vas a poner el url de la imagen grande
@@ -54,8 +46,8 @@ contextInfo: {
 externalAdReply: {
 showAdAttribution: true,
 renderLargerThumbnail: false,
-title: Free Fire,
-body: vs,
+title: `Free Fire`,
+body: `vs`,
 mediaType: 1,
 sourceUrl: 'https://www.instagram.com/ricardoookadm_',
 thumbnailUrl: 'https://telegra.ph/file/0ea56fa1fba7bdbb619ae.jpg'
@@ -64,5 +56,5 @@ thumbnailUrl: 'https://telegra.ph/file/0ea56fa1fba7bdbb619ae.jpg'
 }, { mentions: m.sender })
 
 }
-handler.command = /^(pruebakatas|vs16)$/i
+handler.command = /^(16vs16|vs16)$/i
 export default handler
